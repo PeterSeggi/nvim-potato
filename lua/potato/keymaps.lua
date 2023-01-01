5b65f576543984c8fb2f8a9926d436ec9666bdcb
PeterSeggi <pedro.seggiaro@gmail.com>
2023-01-01 18:29:17 -0300

    Added shift w to go back a word (can still use b)

--- lua/potato/keymaps.lua
+++ lua/potato/keymaps.lua
@@ -25,6 +25,7 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+keymap("n", "<S-w>", "b", opts)
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
