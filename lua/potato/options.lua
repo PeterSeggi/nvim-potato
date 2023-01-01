d143b109fffed4705c0c694da44fd6c5e9b5def6
PeterSeggi <pedro.seggiaro@gmail.com>
2022-12-31 21:07:34 -0300

    Added an autocommand to zz when inserting

--- lua/potato/options.lua
+++ lua/potato/options.lua
@@ -38,5 +38,6 @@ vim.opt.guifont = "monospace:h17"               -- the font used in graphical ne
 vim.opt.shortmess:append "c"
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[autocmd InsertEnter * norm zz]]
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
