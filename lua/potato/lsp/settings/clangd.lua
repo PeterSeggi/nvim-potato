cb6a35214700f4610b403f1c3181bb98d5ec8543
PeterSeggi <pedro.seggiaro@gmail.com>
2023-01-09 20:48:10 -0300

    Removed auto format on exit on c files because it was triggering on every file type

--- lua/potato/lsp/settings/clangd.lua
+++ lua/potato/lsp/settings/clangd.lua
@@ -1,8 +1,8 @@
 local opts = { noremap = true, silent = true }
 
 local settings = {
-  clangd = {
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]],
+  c = {
+    -- vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]],
     vim.api.nvim_set_keymap("n", "<space>h", "<cmd>ClangdSwitchSourceHeader<cr>", opts)
   },
 }
