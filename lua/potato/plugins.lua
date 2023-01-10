7f8fc66ef5e647cb4dba91a9cff69f90a0315964
PeterSeggi <pedro.seggiaro@gmail.com>
2023-01-09 23:14:29 -0300

    Added gitsigns

--- lua/potato/plugins.lua
+++ lua/potato/plugins.lua
@@ -78,6 +78,9 @@ return packer.startup(function(use)
         run = ':TSUpdate'
     }
 
+    -- Github
+    use "lewis6991/gitsigns.nvim" -- github signs
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
