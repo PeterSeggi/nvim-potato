666b3ea086f2889f7ad22684e90e6f1fec556b77
Peter Seggi <pedro.seggiaro@gmail.com>
2023-03-01 12:33:05 -0300

    ...

--- lua/potato/lsp/settings/clangd.lua
+++ lua/potato/lsp/settings/clangd.lua
@@ -2,6 +2,7 @@ local opts = { noremap = true, silent = true }
 
 local settings = {
     c = {
+        -- I think that this was an attempt at formatting C, leave it commented unless you *know* what youre doing
         -- vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]],
         vim.api.nvim_set_keymap("n", "<space>h", "<cmd>ClangdSwitchSourceHeader<cr>", opts)
     },
